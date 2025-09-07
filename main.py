f2fe7ad3074334bb685151984438b909ff3d45a1
cubershark <chow.ashton@gmail.com>
2025-09-07 03:34:23 +0000

    Print statement test commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
 
-
 pygame.init()
 screen = pygame.display.set_mode((480, 360))
 clock = pygame.time.Clock()
@@ -17,12 +16,31 @@ scene_row = 1
 scene_height = 1
 scene_id = scene_row + scene_height * 100
 print(scene_id)
-blocks = [pygame.Rect(0,300,480,60),pygame.Rect(360,240,120,60),pygame.Rect(150,130,120,30),pygame.Rect(0,240,170,60),pygame.Rect(0,190,80,100),pygame.Rect(300,80,120,30)]
-scene_101 = [pygame.Rect(0,300,480,60),pygame.Rect(360,240,120,60),pygame.Rect(150,130,120,30),pygame.Rect(0,240,170,60),pygame.Rect(0,190,80,100),pygame.Rect(300,80,120,30)]
+blocks = [
+    pygame.Rect(0, 300, 480, 60),
+    pygame.Rect(360, 240, 120, 60),
+    pygame.Rect(150, 130, 120, 30),
+    pygame.Rect(0, 240, 170, 60),
+    pygame.Rect(0, 190, 80, 100),
+    pygame.Rect(300, 80, 120, 30)
+]
+scene_101 = [
+    pygame.Rect(0, 300, 480, 60),
+    pygame.Rect(360, 240, 120, 60),
+    pygame.Rect(150, 130, 120, 30),
+    pygame.Rect(0, 240, 170, 60),
+    pygame.Rect(0, 190, 80, 100),
+    pygame.Rect(300, 80, 120, 30)
+]
+print("GAME START")
+
+
 def set_up(scene):
     pass
+
+
 def fix_overlap():
-    global velocity_y,player_y,fall_tick
+    global velocity_y, player_y, fall_tick
     if velocity_y > 0:
         player_y -= velocity_y
         velocity_y = 0
@@ -30,36 +48,38 @@ def fix_overlap():
     elif velocity_y < 0:
         player_y -= velocity_y
         velocity_y = 0
+
+
 def move(x):
-    global velocity_x,player_x,player_y
+    global velocity_x, player_x, player_y
     player_x += x
     player_y -= 3
-    player = pygame.draw.rect(screen, "red", (player_x,player_y,50,50))
+    player = pygame.draw.rect(screen, "red", (player_x, player_y, 50, 50))
     for block in blocks:
         if player.colliderect(block):
             player_x -= x
-            player = pygame.draw.rect(screen, "red", (player_x,player_y,50,50))
+            player = pygame.draw.rect(screen, "red",
+                                      (player_x, player_y, 50, 50))
             velocity_x = 0
     player_y += 3
 
+
 while running:
-   
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-   
     screen.fill("blue")
 
     velocity_x *= 0.6
-    player = pygame.draw.rect(screen, "red", (player_x,player_y,50,50))
-    floor = pygame.draw.rect(screen, "purple", (0,300,480,60))
+    player = pygame.draw.rect(screen, "red", (player_x, player_y, 50, 50))
+    floor = pygame.draw.rect(screen, "purple", (0, 300, 480, 60))
     for block in blocks:
         pygame.draw.rect(screen, "purple", block)
     for block in blocks:
         if player.colliderect(block):
             fix_overlap()
-        
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP] and fall_tick == 0:
@@ -72,14 +92,11 @@ while running:
         velocity_x += move_speed
     velocity_y -= gravity
 
- 
     pygame.display.flip()
     player_y += velocity_y
     move(velocity_x)
     fall_tick += 1
 
- 
     dt = clock.tick(60) / 1000
-    
 
 pygame.quit()
